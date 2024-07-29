72cda1d9a6b5055fcf9ab73f3fa1739ed24ac175
RetroPipes <eric.ahnell@puttysoftware.com>
2024-07-29 15:47:19 -0400

    Game Object Data Loader: Fully package-protect.

--- dd7-game-object/src/org/retropipes/dungeondiver7/gameobject/GameObjectDataLoader.java
+++ dd7-game-object/src/org/retropipes/dungeondiver7/gameobject/GameObjectDataLoader.java
@@ -5,17 +5,17 @@ import java.util.ResourceBundle;
 import org.retropipes.dungeondiver7.loader.image.gameobject.ObjectImageId;
 
 final class GameObjectDataLoader {
-    public static boolean friction(final ObjectImageId index) {
+    static boolean friction(final ObjectImageId index) {
 	return (boolean) ResourceBundle.getBundle("asset.data.gameobject.friction")
 		.getObject(Integer.toString(index.ordinal()));
     }
 
-    public static int layer(final ObjectImageId index) {
+    static int layer(final ObjectImageId index) {
 	return (int) ResourceBundle.getBundle("asset.data.gameobject.layer")
 		.getObject(Integer.toString(index.ordinal()));
     }
 
-    public static boolean solid(final ObjectImageId index) {
+    static boolean solid(final ObjectImageId index) {
 	return (boolean) ResourceBundle.getBundle("asset.data.gameobject.solid")
 		.getObject(Integer.toString(index.ordinal()));
     }
